95cd479b45b37289c7e7b20ca4f8f0fc29c376a7
LegMateo <mateo.legovic1@gmail.com>
2024-02-28 14:13:00 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -8,6 +8,55 @@ import "@/fonts/bootstrap-icons.woff2";
 import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
 import App from "./App.vue";
+(function ($) {
+  "use strict";
+
+  // NAVBAR
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  $(function () {
+    $(".hero-slides").vegas({
+      slides: [
+        {
+          src: "https://imgur.com/5nlRJizjpeg",
+        },
+        {
+          src: "https://imgur.com/5nlRJiz.jpeg",
+        },
+        {
+          src: "https://imgur.com/5nlRJiz.jpeg",
+        },
+      ],
+      timer: false,
+      animation: "kenburns",
+    });
+  });
+
+  // CUSTOM LINK
+  $(".smoothscroll").click(function () {
+    var el = $(this).attr("href");
+    var elWrapped = $(el);
+    var header_height = $(".navbar").height() + 60;
+
+    scrollToDiv(elWrapped, header_height);
+    return false;
+
+    function scrollToDiv(element, navheight) {
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop - navheight;
+
+      $("body,html").animate(
+        {
+          scrollTop: totalScroll,
+        },
+        300
+      );
+    }
+  });
+})(window.jQuery);
 
 const messages = {
   en: {
